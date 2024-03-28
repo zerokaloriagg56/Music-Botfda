43a64658aeb5c6de8c74b50ead27ec2f01989444
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-03-28 15:34:05 +0100

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -32,7 +32,7 @@ module.exports = {
       queue.stop(interaction.guild.id);
 
       const embed = new EmbedBuilder()
-        .setColor('#f1002c')
+        .setColor('#000000')
         .setAuthor({
           name: 'Music Stopped',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
