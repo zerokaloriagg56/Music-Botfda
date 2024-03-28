2cf09ce53e636b4fd2fe3a1ce30f68ceb5505d72
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-03-28 15:27:37 +0100

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -58,7 +58,7 @@ try {
   const success = await queue.skip();
 
   const embed = new EmbedBuilder()
-    .setColor('#3498db')
+    .setColor('#000000')
     .setAuthor({
       name: 'Song Skipped',
       iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
