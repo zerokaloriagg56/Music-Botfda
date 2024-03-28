4f9255ab924943a2f471658d350eed493b9f34a0
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-03-28 15:28:13 +0100

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -43,7 +43,7 @@ module.exports = {
       )
 
       const embed = new EmbedBuilder()
-        .setColor('#fc4e03')
+        .setColor('#000000')
         .setAuthor({
         name: 'Loop Your Melodies',
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157318080670728283/7905-repeat.gif?ex=65182bf4&is=6516da74&hm=9ae58f40fcea5dc42a2a992bbd159d50116b3bafe5c5f7728e3a5276442efd2a&', 
