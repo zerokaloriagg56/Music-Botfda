f60af34a7850f683e35c2925faae9d32e9f81ca6
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-03-28 13:31:24 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -23,8 +23,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 setInterval(() => client.user.setActivity({ 
   name: `FurYxMusic.gg`, 
-  type: ActivityType.STREAMING }), 10000),
-  url: 'https://www.youtube.com/watch?v=K75KmobaNbQ';
+  type: ActivityType.LISTENING }), 10000),
     
 client.errorLog = config.errorLog
 } else {
