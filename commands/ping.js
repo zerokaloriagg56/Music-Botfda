ea6e88e59d20eda7731ccad5ad565f0b0df318fb
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-03-28 15:32:45 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -29,7 +29,7 @@ module.exports = {
       interaction.reply("Pinging....").then(msg => {
         const end = Date.now();
         const embed = new EmbedBuilder()
-          .setColor(`#6190ff`)
+          .setColor(`#000000`)
           .setTitle(`Bot Latency`)
           .setDescription(`**Pong** : ${end - start}ms`)
         return interaction.editReply({ embeds: [embed] }).catch(e => { });
