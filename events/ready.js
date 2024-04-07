136f4935e72be3a66b998204f43fe9323f2a84f4
ZroEdwardo <166321794+ZroEdwardo@users.noreply.github.com>
2024-04-07 21:18:03 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -21,9 +21,11 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
-setInterval(() => client.user.setActivity: [{ name: `FurYxMusic.gg`, type: ActivityType.Listening }],
-      status: 'idle',
-    });
+setInterval(() => client.user.setActivity({ 
+      status: 'online',
+      activity: {
+        name: 'Test',
+        type: 'PLAYING',
 
 client.errorLog = config.errorLog
 } else {
