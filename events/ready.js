e691d94c2df932076bd7ec1f3d2f2d3594e8d633
ZroEdwardo <166321794+ZroEdwardo@users.noreply.github.com>
2024-04-07 21:23:38 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -26,7 +26,7 @@ client.user.setPresence({
         name: 'FurYxHosted.gg', 
         type: ActivityType.Listening,
     }], 
-    status: 'online' 
+    status: 'idle' 
 });
 
 client.errorLog = config.errorLog
