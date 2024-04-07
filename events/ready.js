45ce7634e2bbbb4fd0e08e0fe711941f8ebeaf22
ZroEdwardo <166321794+ZroEdwardo@users.noreply.github.com>
2024-04-07 21:10:35 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -21,10 +21,10 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
-      client.user.setActivity: [{ name: `FurYxMusic.gg`, type: ActivityType.Listening }],
+setInterval(() => client.user.setActivity: [{ name: `FurYxMusic.gg`, type: ActivityType.Listening }],
       status: 'idle',
     });
-    
+
 client.errorLog = config.errorLog
 } else {
 console.log('\x1b[36m%s\x1b[0m', `|    🍔 Error MongoDB!`)
